e69a440b9826e89543a31bb19d9fff6b71bdc66e
Tatiana <bodtata@yandex.ru>
2025-03-10 13:46:23 +0100

    feat: solve fromJSON task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -216,8 +216,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = Object.create(proto);
+  const data = JSON.parse(json);
+  Object.assign(obj, data);
+  return obj;
 }
 
 /**
